27397ae3b2ef407dd8dfad6e2645d73a2e114336
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-11-07 11:08:28 +0100

    dev: added express example

---
+++ back/server.js
@@ -0,0 +1,19 @@
+console.log("About to start a server...");
+const express = require("express");
+console.log("express: ", express);
+
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  console.log("req: ", req.url);
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
