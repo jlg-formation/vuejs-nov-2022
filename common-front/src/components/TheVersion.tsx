e993a56a1bc5ca6d032bf757b4741b045df67687
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-11-09 16:24:19 +0100

    dev: version jsx

--- common-front/src/components/TheVersion.tsx
+++ common-front/src/components/TheVersion.tsx
@@ -1,8 +1,15 @@
 import { version } from "../../package.json";
 
-export default ({ frontVersion }: { frontVersion: string }) => (
+const truc = [1, 2];
+
+export default ({ frontVersion }: { frontVersion?: string }) => (
   <div title="coucou">
     <p>Version projet commun: {version}</p>
-    <p>Version Front: {frontVersion}</p>
+    {frontVersion !== undefined && <p>Version Front: {frontVersion}</p>}
+    {truc.map((n) => (
+      <div>
+        <span>{n}</span>
+      </div>
+    ))}
   </div>
 );
